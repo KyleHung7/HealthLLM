4a7c93a9d78f445db44f4b5e3e6b36023bd322af
sk5s <samko5sam@tutanota.com>
2025-05-12 10:43:01 +0800

    Feat: Add cache control headers to general user dashboard response

--- healthanaly/app.py
+++ healthanaly/app.py
@@ -114,7 +114,11 @@ def general_dashboard():
 
     if account_role == 'general':
         # Only general users can access this dashboard.
-        return render_template('general_user_dashboard.html')
+        response = make_response(render_template('general_user_dashboard.html'))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
     else:
         # If an elderly user or any other role tries to access, redirect to their default page.
         return redirect(url_for('index'))
